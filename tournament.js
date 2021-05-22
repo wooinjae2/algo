eef4ef5151d0b5786152d2f2b552e47a28927ecb
31 <monsterfairy2@gmail.com>
2021-05-22 16:31:36 +0900

    예상대진표 추가

---
+++ tournament.js
@@ -0,0 +1,17 @@
+function solution(n,a,b)
+{
+    var answer = 0;
+
+    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    while(a !== b){
+        a = Math.ceil(a/2);
+        b = Math.ceil(b/2);
+        answer++;
+    }
+    console.log(answer);
+    return answer;
+}
+
+
+solution(8,	4,	7)
+solution(8,	1,	2);
