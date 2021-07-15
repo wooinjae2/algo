a84d56cb5f228244430c9149a78c84323ab197fd
wooinjae <monsterfairy2@gmail.com>
2021-07-15 23:53:16 +0900

    Update tournament.js

--- tournament.js
+++ tournament.js
@@ -3,12 +3,14 @@ function solution(n,a,b)
     var answer = 0;
 
     // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+    console.log(n,a,b);
     while(a !== b){
         a = Math.ceil(a/2);
         b = Math.ceil(b/2);
         answer++;
     }
     console.log(answer);
+    
     return answer;
 }
 
